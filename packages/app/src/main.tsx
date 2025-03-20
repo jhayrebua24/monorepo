7239fd7dd39af521474b58258c7129e2f457bb86
jei rebua <jhayrebua123@gmail.com>
2025-03-20 17:02:40 +0800

    remove session provider in main

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { SessionProvider } from "@acme/common";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SessionProvider>
-      <App />
-    </SessionProvider>
+    <App />
   </StrictMode>
 );
